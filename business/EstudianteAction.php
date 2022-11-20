b8efe9a86326d3720c1d2661fff3562624f760b3
Yery Steven Marin Ortiz <111812790+YerySteven@users.noreply.github.com>
2022-11-19 18:48:39 -0600

    Arreglo

--- business/EstudianteAction.php
+++ business/EstudianteAction.php
@@ -5,16 +5,17 @@ include 'EstudianteBusiness.php';
 
 
 if(isset($_POST['Insertar'])){
-    if (isset($_POST['est_Nombre']) && isset($_POST['est_Apellido1'])
+    if (isset($_POST['est_Cedula']) && isset($_POST['est_Nombre']) && isset($_POST['est_Apellido1'])
     && isset($_POST['est_Apellido2'])&& isset($_POST['est_direccion'])&& isset($_POST['est_TipoBeca'])
 ) {
- $nombre = $_POST['est_Nombre'];
+ $est_Cedula = $_POST['est_Cedula'];
+ $est_Nombre = $_POST['est_Nombre'];
  $apellido1 = $_POST['est_Apellido1'];
  $apellido2 = $_POST['est_Apellido2'];
  $est_direccion = $_POST['est_direccion'];
  $est_TipoBeca = $_POST['est_TipoBeca'];
  
- $estudiante = new Estudiante(0,$est_Nombre, $est_Apellido1, $est_Apellido2,$est_direccion,$est_TipoBeca);
+ $estudiante = new Estudiante($est_Cedula,$est_Nombre, $est_Apellido1, $est_Apellido2,$est_direccion,$est_TipoBeca);
  
  $estudianteBusiness = new EstudianteBusiness();
  $resultado = $estudianteBusiness->insertar($estudiante);
